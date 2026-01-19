36ac1d79ab57e6d6facb8e4643f235e031f5615a
vansh-verma-dev <vanshverma6065@gmail.com>
2026-01-19 11:05:49 +0530

    desgin dashboard page

--- JS/auth.js
+++ JS/auth.js
@@ -2,3 +2,29 @@ function loginpage() {
   window.location.href = "login.html";
 }
 
+const ctx = document.getElementById('pieChart');
+
+new Chart(ctx, {
+  type: 'pie',
+  data: {
+    labels: ['Sales', 'Purchase', 'Profit'],
+    datasets: [{
+      data: [45000, 30000, 15000],
+      backgroundColor: [
+        '#4e73df',
+        '#1cc88a',
+        '#f6c23e'
+      ]
+    }]
+  },
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'bottom'
+      }
+    }
+  }
+});
+ 
+
